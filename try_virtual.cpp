57c5cfdef5b9fd794815c040dd3feeb159fe515a
viml kumar <vimalkg@gmail.com>
2022-11-29 06:09:19 +0530

    removed blanks

--- try_virtual.cpp
+++ try_virtual.cpp
@@ -5,57 +5,30 @@
 using namespace std;
 
 class Animal {
-   private:
-    string type;
-
+   private: string type;
    public:
-    // constructor to initialize type
     Animal() : type("Animal") {}
-
-    // declare virtual function
-    virtual string getType() {
-        return type;
-    }
+    virtual string getType() { return type; }
 };
-
 class Dog : public Animal {
-   private:
-    string type;
-
+   private: string type;
    public:
-    // constructor to initialize type
     Dog() : type("Dog") {}
-
-    string getType() override {
-        return type;
-    }
+    string getType() override { return type; }
 };
-
 class Cat : public Animal {
-   private:
-    string type;
-
+   private: string type;
    public:
-    // constructor to initialize type
     Cat() : type("Cat") {}
-
-    string getType() override {
-        return type;
-    }
+    string getType() override { return type; }
 };
-
-void print(Animal* ani) {
-    cout << "Animal: " << ani->getType() << endl;
-}
-
+void print(Animal* ani) { cout << "Animal: " << ani->getType() << endl; } // late binding
 int main() {
     Animal* animal1 = new Animal();
     Animal* dog1 = new Dog();
     Animal* cat1 = new Cat();
-
     print(animal1);
     print(dog1);
     print(cat1);
-
     return 0;
 }
